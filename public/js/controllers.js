21a54968aed79a7aacacae9eaa95257f03c63f3a
Johann <git@johann-hofmann.com>
2013-07-06 20:31:37 +0200

    spaeti overview

---
+++ public/js/controllers.js
@@ -0,0 +1,72 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+  .controller('SpaetiListCtrl', function ($scope, $http) {
+    function refresh() {
+      $http.get("http://spaeti.pavo.uberspace.de/dev/spaeti/")
+        .success(function (data) {
+          $scope.spaetis = data;
+        });
+    }
+
+    refresh();
+
+    $scope.remove = function (spaeti) {
+      if(spaeti !== undefined) {
+        $scope.spaetiToDelete = spaeti;
+        return;
+      }
+      $http.delete("http://spaeti.pavo.uberspace.de/dev/spaeti/" + $scope.spaetiToDelete._id)
+        .success(function () {
+          $scope.lastDeletedSpaeti = $scope.spaetiToDelete;
+          refresh();
+        }).error(function (data, status) {
+          alert("Delete failed:" + status);
+        });
+    };
+
+    $scope.restoreLastDeletedSpaeti = function () {
+      $http.post("http://spaeti.pavo.uberspace.de/dev/spaeti/", $scope.lastDeletedSpaeti)
+        .success(refresh).error(function (data, status) {
+          alert("Creation failed:" + status);
+        });
+    };
+
+    $scope.update = function (spaeti, attr) {
+      $http.put("http://spaeti.pavo.uberspace.de/dev/spaeti/" + spaeti._id, spaeti)
+        .success(refresh).error(function (data, status) {
+          alert("Update failed:" + status);
+        });
+      spaeti.edit[attr] = false;
+    };
+
+    $scope.edit = function (spaeti, attr) {
+      if(!spaeti.edit) {
+        spaeti.edit = {};
+      }
+      spaeti.edit[attr] = true;
+    };
+
+    $scope.cancelEdit = function (spaeti, attr) {
+      spaeti.edit[attr] = false;
+    };
+  })
+  .controller('SpaetiDetailCtrl', function ($scope, $http) {
+    function update() {
+      $http.get("http://spaeti.pavo.uberspace.de/dev/spaeti/")
+        .success(function (data) {
+          $scope.spaetis = data;
+        });
+    }
+
+    update();
+
+    $scope.remove = function (spaeti) {
+      $http.delete("http://spaeti.pavo.uberspace.de/dev/spaeti/" + spaeti._id)
+        .success(update).error(function (data, status) {
+          alert("Delete failed:" + status);
+        });
+    };
+  });
